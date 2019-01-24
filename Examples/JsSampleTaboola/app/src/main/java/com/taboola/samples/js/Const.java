3b095aae1cc60c82199a0b74c9d7c116e6162417
omria-taboola <omri.a@taboola.com>
2019-01-24 13:11:40 +0200

    Removed old example project

--- Examples/JsSampleTaboola/app/src/main/java/com/taboola/samples/js/Const.java
+++
@@ -1,9 +0,0 @@
-package com.taboola.samples.js;
-
-public class Const {
-
-    public static final String BASE_URL = "http://example.com";
-
-    public static final String CONTENT_HTML_FILE_NAME = "content_html.html";
-
-}
